19645f9d996f332c986c75276360c18c45724b20
Sebas4120 <144409264+Sebas4120@users.noreply.github.com>
2024-06-09 08:00:56 -0400

    Sunday

--- src/main/java/ca/georgiancollege/assignment1gc8953/PopulationController.java
+++ src/main/java/ca/georgiancollege/assignment1gc8953/PopulationController.java
@@ -1,4 +1,21 @@
 package ca.georgiancollege.assignment1gc8953;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+
 public class PopulationController {
+
+    @FXML
+    private BarChart<Number, Number> OntarioChart;
+
+    @FXML
+    void initialize() {
+
+        XYChart.Series<Number, Number> data = new XYChart.Series<>();
+
+        data.getData().add(new XYChart.Data<>(2011, 12851821));
+    }
+
+
 }
