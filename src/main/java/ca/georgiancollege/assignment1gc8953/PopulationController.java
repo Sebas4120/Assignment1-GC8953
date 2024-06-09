84bf700024cc823cbc2aa10ff546f97c33f65695
Sebas4120 <144409264+Sebas4120@users.noreply.github.com>
2024-06-09 08:26:27 -0400

    Sunday
    Tengo el grafico de inicio donde muestra la poblacion de Ontario desde 2020 hasta 2024

--- src/main/java/ca/georgiancollege/assignment1gc8953/PopulationController.java
+++ src/main/java/ca/georgiancollege/assignment1gc8953/PopulationController.java
@@ -4,17 +4,42 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PopulationController {
 
     @FXML
-    private BarChart<Number, Number> OntarioChart;
+    private BarChart<String, Number> OntarioChart;
 
     @FXML
     void initialize() {
 
-        XYChart.Series<Number, Number> data = new XYChart.Series<>();
+        XYChart.Series<String, Number> data = new XYChart.Series<>();
 
-        data.getData().add(new XYChart.Data<>(2011, 12851821));
+        try(
+                Connection connection = DriverManager.getConnection("jdbc:mysql://database-1" +
+                        ".cpiiw6m2micq.us-east-2.rds.amazonaws.com:3306/COMP1011", "admin",
+                "Narangita412.");
+            Statement statement = connection.createStatement()){
+
+            String query = "SELECT Year, Population FROM ONTARIO";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                String year = resultSet.getString("Year");
+                int population = resultSet.getInt("Population");
+                data.getData().add(new XYChart.Data<>(year, population));
+            }
+
+        } catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        OntarioChart.getData().add(data);
     }
 
 
