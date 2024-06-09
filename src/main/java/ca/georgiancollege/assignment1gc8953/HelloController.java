7bf08ba720413f28494422c7365b302b5d297fa3
Sebas4120 <144409264+Sebas4120@users.noreply.github.com>
2024-06-08 21:48:41 -0400

    Test

---
+++ src/main/java/ca/georgiancollege/assignment1gc8953/HelloController.java
@@ -0,0 +1,14 @@
+package ca.georgiancollege.assignment1gc8953;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.georgiancollege.assignment1gc8953 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.georgiancollege.assignment1gc8953 to javafx.fxml;
+    exports ca.georgiancollege.assignment1gc8953;
+}
